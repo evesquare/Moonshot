f56871b264a9b7e929f28a9ad4ba35705d96a6a6
evesquare <50450074+EveSquare@users.noreply.github.com>
2025-04-27 20:10:59 +0900

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by evesquare on 2025/04/27.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
