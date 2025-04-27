18723b4caa213e06d8031c8a9f0fb57e16b51ffb
evesquare <50450074+EveSquare@users.noreply.github.com>
2025-04-27 22:40:26 +0900

    Add astronaut and mission data models, views, and assets
    - Created Astronaut and Mission structs for data representation.
    - Implemented AstronautView and MissionView for displaying astronaut and mission details.
    - Added Bundle extension for decoding JSON files.
    - Introduced custom color themes for dark and light backgrounds.
    - Updated ContentView to display missions in a grid layout with navigation links.
    - Added JSON files for astronauts and missions data.
    - Included image assets for astronauts and missions.

---
+++ Moonshot/AstronautView.swift
@@ -0,0 +1,36 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by evesquare on 2025/04/27.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(astronaut.description)
+                    .padding()
+            }
+            
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    AstronautView(astronaut: astronauts["aldrin"]!)
+        .preferredColorScheme(.dark)
+}
